3a96d819ddfeba409d62a0e9a5c2b91f93bf0a46
srthurman <srthurman@gmail.com>
2015-11-06 21:26:08 +0000

    Put controller code in mapController.js, add function to find a block given an x,y coord

--- app/js/controllers/mapController.js
+++ app/js/controllers/mapController.js
@@ -1,7 +1,12 @@
 'use strict';
 
-app.controller("LayersEsriDynamicLayerController", [ "$scope", function($scope) {
+app.controller("LayersEsriDynamicLayerController", [ "$scope", '$http', function($scope, $http) {
             angular.extend($scope, {
+                blockLayer: "https://tigerweb.geo.census.gov/arcgis/rest/services/TIGERweb/Tracts_Blocks/MapServer/2/query",
+                blockVars: {
+                    xcoord: -77.016,
+                    ycoord: 38.904
+                },
                 dc: {
 	            	lat: 38.904,
 	                lng: -77.016,
@@ -43,5 +48,19 @@ app.controller("LayersEsriDynamicLayerController", [ "$scope", function($scope) 
 				    	},
                     }
                 },
+                
+                findBlockLayer: function(blockVars) {
+                    if (blockVars.hasOwnProperty('xcoord') && blockVars.hasOwnProperty('ycoord')) {
+                        var endURL = "+&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=json";
+                        return [$scope.blockLayer,"?where=&text=&objectIds=&time=&geometry=",blockVars.xcoord,"+%2C+",blockVars.ycoord,endURL].join('');
+                    }
+                }
+            });
+            
+            $http.get($scope.findBlockLayer($scope.blockVars)).success(function(data, status) {
+                alert("success");
+                console.log(status);
+                console.log("data");
+                console.log(data.features[0].geometry);
             });
         }]);
